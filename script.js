8b29dd739f4c8b7b84d350571605f74842b5db3c
AlexSaxena <saxenaalex@gmail.com>
2022-01-26 22:36:17 +0100

    Adds Color trailing function, Adds btn event Clear

--- script.js
+++ script.js
@@ -3,8 +3,37 @@ console.log("Greetings, General Kenobi");
 let gridContainer = document.getElementById("grid-container");
 let btnErase = document.getElementById("btn-erase");
 let btnClear = document.getElementById("btn-clear");
+let boxes = document.getElementsByClassName("generatedBox2");
 
-addBoxes(4);
+let c = 16;
+let fullRange = c * c;
+
+addBoxes(c);
+
+//changeColor();
+trailing(fullRange);
+
+// Function for Colour trailing over Divs
+function trailing(i) {
+  for (let a = 0; a < i; a++) {
+    boxes.item(a).addEventListener("mouseover", () => {
+      boxes.item(a).style.backgroundColor = "red";
+    });
+  }
+}
+
+// Btn Event to change colour of all boxes to white
+btnClear.addEventListener("click", () => {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = "white";
+  }
+});
+
+// function changeColor() {
+//   for (var i = 0; i < boxes.length; i++) {
+//     boxes[i].style.backgroundColor = "black";
+//   }
+// }
 
 // Appending Boxes to Container
 function addBoxes(nrOfBoxes) {
