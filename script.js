195f9c3769d73e0d39997e90b15751cecd990cc2
AlexSaxena <saxenaalex@gmail.com>
2022-01-26 17:26:13 +0100

    Adds function->calling &Appending div to container

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ let gridContainer = document.getElementById("grid-container");
 let btnErase = document.getElementById("btn-erase");
 let btnClear = document.getElementById("btn-clear");
 
+addBoxes(4);
+
+// Appending Boxes to Container
+function addBoxes(nrOfBoxes) {
+  for (let i = 0; i < nrOfBoxes; i++) {
+    let box = newDiv();
+
+    gridContainer.appendChild(box);
+
+    for (let j = 0; j < nrOfBoxes; j++) {
+      let box2 = newDiv2();
+      box.appendChild(box2);
+    }
+  }
+}
+
 // Create a new Div With class generatedBox
 function newDiv() {
   let newDiv = document.createElement("div");
