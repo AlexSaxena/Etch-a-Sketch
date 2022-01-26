4f352204c1ee8d25a33fdec5133a25bcdfa05796
AlexSaxena <saxenaalex@gmail.com>
2022-01-26 17:23:53 +0100

    Adds Functions for generating Divs

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Greetings, General Kenobi");
+
+let gridContainer = document.getElementById("grid-container");
+let btnErase = document.getElementById("btn-erase");
+let btnClear = document.getElementById("btn-clear");
+
+// Create a new Div With class generatedBox
+function newDiv() {
+  let newDiv = document.createElement("div");
+  newDiv.classList = "generatedBox";
+  return newDiv;
+}
+function newDiv2() {
+  let newDiv = document.createElement("div");
+  newDiv.classList = "generatedBox2";
+  return newDiv;
+}
